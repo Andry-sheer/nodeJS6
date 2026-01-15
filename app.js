b68d9fa46dad54e0c85434910624988e290b8b08
Andrew <Andry.sheremetew@gmail.com>
2026-01-15 16:27:34 +0200

    init project

---
+++ app.js
@@ -0,0 +1,11 @@
+import express from 'express'
+import dotenv from 'dotenv'
+dotenv.config();
+
+const PORT = process.env.PORT || 3001
+
+const server = express();
+
+server.listen(PORT, ()=> {
+  console.log(`server launched http://localhost:${PORT}`);
+})

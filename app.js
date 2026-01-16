9a95b8733aebe2adddbb643460f04b141a38c9cc
Andrew <Andry.sheremetew@gmail.com>
2026-01-16 16:26:13 +0200

    add pug + add users + add fav + pages

--- app.js
+++ app.js
@@ -1,11 +1,40 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import morgan from 'morgan';
+import session from 'express-session';
 dotenv.config();
 
 const PORT = process.env.PORT || 3001
 
-const server = express();
+const app = express();
 
-server.listen(PORT, ()=> {
-  console.log(`server launched http://localhost:${PORT}`);
+app.set('view engine', 'pug');
+app.set('views', './views');
+app.use(morgan('dev'));
+
+app.use(express.static('./public'));
+app.use(express.static('./static'));
+
+
+app.get('/', (req, res)=> {
+  res.render('index', {
+    title: 'Home'
+  })
 })
+
+app.get('/about', (req, res)=> {
+  res.render('about', {
+    title: 'About'
+  })
+})
+
+
+
+app.use((req, res)=> {
+  res.status(404).send('page not found | 404')
+})
+
+//! додав емодзі запуску))) (про ракету аж страшно писати... 🥲)
+app.listen(PORT, ()=> {
+  console.log(`✅ Server launched at http://localhost:${PORT}`);
+});
